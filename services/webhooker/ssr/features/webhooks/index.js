327cb5a7fb268237bf525847ee59f6c75912e5be
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-03-15 04:40:41 +0100

    add fixtures

--- services/webhooker/ssr/features/webhooks/index.js
+++ services/webhooker/ssr/features/webhooks/index.js
@@ -1,8 +1,16 @@
-import { EXPRESS_ROUTE } from 'services/express/hooks'
+import bodyParser from 'body-parser'
+import { EXPRESS_MIDDLEWARE, EXPRESS_ROUTE } from 'services/express/hooks'
 import { FEATURE_NAME } from './hooks'
 import { makeWebhookRoute } from './webhook-route'
 
-export const register = ({ registerAction, settings }) =>
+export const register = ({ registerAction, settings }) => {
+    registerAction({
+        hook: EXPRESS_MIDDLEWARE,
+        name: FEATURE_NAME,
+        trace: __filename,
+        handler: ({ app }) =>
+            app.use(bodyParser.json()),
+    })
     registerAction({
         hook: EXPRESS_ROUTE,
         name: FEATURE_NAME,
@@ -10,3 +18,5 @@ export const register = ({ registerAction, settings }) =>
         handler: ({ app }) =>
             app.post('/', makeWebhookRoute(settings)),
     })
+
+}
--- services/webhooker/ssr/features/webhooks/webhook-route.js
+++ services/webhooker/ssr/features/webhooks/webhook-route.js
@@ -1,4 +1,38 @@
+import path from 'path'
+import fs from 'fs-extra'
 
 export const makeWebhookRoute = (settings) => {
-    return (req, res) => res.send(settings)
+    return (req, res) => {
+        console.log('POST!')
+
+        let fixtureName = `${req.headers['x-github-event']}`
+        switch (req.headers['x-github-event']) {
+            case 'issues':
+                fixtureName += `--${req.body.action}`
+                break
+        }
+
+        // Change in comment
+        if (fixtureName === 'issues--edited') {
+            try {
+                if (req.body.comment.id) {}
+                fixtureName += '--comment'
+            } catch (err) {}
+        }
+
+        fs.writeJSONSync(path.join(__dirname, 'fixtures_new', `${fixtureName}.json`), {
+            request: {
+                url: req.protocol + '://' + req.get('host') + req.originalUrl,
+                from: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+            },
+            headers: req.headers,
+            body: req.body,
+        }, { spaces: 4 })
+
+        res.send({
+            settings,
+            body: req.body,
+            headers: req.headers,
+        })
+    }
 }
